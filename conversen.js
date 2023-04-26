0e0b7cc0f28db76bdab2c97e416d6368d91788d5
rabbani4u <raabbani.iu31@gmail.com>
2023-04-26 23:26:47 +0600

    update

---
+++ conversen.js
@@ -0,0 +1,8 @@
+function calculate(inch) {
+  let feet = inch / 12;
+  return feet;
+}
+let output = calculate(36);
+let output1 = calculate(48);
+console.log(output);
+console.log(output1);
--- function.js
+++ function.js
@@ -1,6 +1,22 @@
-function double(x) {
-  let y = x * 2;
-  return y;
-}
-let first = double(50);
-console.log(first);
+// function double(x) {
+//   let y = x * 2;
+//   return y;
+// }
+// let first = double(50);
+// console.log(first);
+
+// function add(num1, num2) {
+//   let result = num1 + num2;
+//   return result;
+// }
+// let sum = add(10, 20);
+// console.log(sum);
+
+let student = {
+  id: 1,
+  name: "Rabbani",
+  roll: 007,
+  regno: 1305,
+};
+
+console.log(student.name);
